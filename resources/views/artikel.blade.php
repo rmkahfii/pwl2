439e4490047e82765ef63444183dfc71100e8ef4
rmkahfii <kahfiiskandar90@gmail.com>
2026-03-29 17:27:24 +0700

    firs comit

---
+++ resources/views/artikel.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Artikel</title>
+</head>
+<body>
+     <nav><a href="/">Home</a></nav>
+    <nav><a href="/about">About</a></nav>
+    <nav><a href="/artikel">Artikel</a></nav>
+    <nav><a href="/contact">Contact</a></nav>
+
+    <article>
+        <h1>Judul 1</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quaerat! Omnis mollitia obcaecati molestiae ut, sit adipisci unde temporibus facilis autem itaque est, labore ipsam optio! Cupiditate sint iste ullam.</p>
+        
+    </article>
+    <article>
+        <h1>Judul 2</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sit voluptates dolores quos itaque blanditiis cupiditate labore eaque quod voluptate doloremque nulla molestias ratione laboriosam recusandae aliquam suscipit, laborum soluta?</p>
+        
+    </article>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <nav><a href="/">Home</a></nav>
+    <nav><a href="/about">About</a></nav>
+    <nav><a href="/artikel">Artikel</a></nav>
+    <nav><a href="/contact">Contact</a></nav>
+    <h1>HUBUNGI KAMI</h1>
+    <ul>
+        <li>
+            Instagram: <a href="https://instagram.com/rmkahfii">@rmkahfii</a>
+        </li>
+    </ul>
+
+    <ul>
+        <li>
+            Whatsapp: <a href="https://wa.me/6287792081901">rmkahfii</a>
+        </li>
+    </ul>
+
+</body>
+</html>
