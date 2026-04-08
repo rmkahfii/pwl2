dfb1942718a19a52e878815c71b0bd8797478bce
agis16 <sarifan176@gmail.com>
2026-04-08 21:39:36 +0700

    add: reusable card component

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -20,7 +20,7 @@
   ```
 -->
 <div class="min-h-full">
-  
+
 @include('partials.header')
   <header class="relative bg-white shadow-sm">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
