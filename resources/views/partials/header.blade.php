fd2df1f7554cfaf4cc0a8ddc976d8fc5e45ac4b9
rmkahfii <kahfiiskandar90@gmail.com>
2026-04-09 00:11:22 +0700

    udpate sidebar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -49,9 +49,22 @@
               Your profile
               </a>
 
-              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-              Settings
+              <a href="/" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+              Dashboard
               </a>
+
+               <a href="/artikel" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+              Artikel
+              </a>
+
+               <a href="/about" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+              About
+              </a>
+
+               <a href="/contact" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+              Contact
+              </a>
+
 
               <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
               Sign out
@@ -105,7 +118,8 @@
         </div>
         <div class="mt-3 space-y-1 px-2">
           <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Your profile</a>
-          <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Settings</a>
+          <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Home</a>
+           <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Settings</a>
           <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Sign out</a>
         </div>
       </div>
