805e30c38a7605a934d91c0c9da7b3f3a9ae549b
rmkahfii <kahfiiskandar90@gmail.com>
2026-04-09 14:06:42 +0700

    update halaman sidebar2

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,69 +1,69 @@
 <div class="flex min-h-screen">
 
-<!-- SIDEBAR -->
-<div class="w-64 bg-gray-800 text-white">
+  <!-- SIDEBAR -->
+  <div class="w-64 bg-gray-800 text-white">
 
-<div class="flex items-center justify-center h-16 border-b border-gray-700">
-<img src="img/man.png" class="size-9">
-<span class="ml-2 font-bold"></span>
-</div>
+    <div class="flex items-center justify-center h-16 border-b border-gray-700">
+      <img src="img/man.png" class="size-9">
+      <span class="ml-2 font-bold"></span>
+    </div>
 
-<div class="p-4 space-y-2">
+        <div class="p-4 space-y-2">
 
-<a href="/" class="block px-4 py-2 rounded bg-gray-900">
-Dashboard
-</a>
+          <a href="/" class="block px-4 py-2 rounded bg-gray-900">
+            Dashboard
+          </a>
 
-<a href="/list-data" class="block px-4 py-2 rounded hover:bg-gray-700">
-List Data
-</a>
+          <a href="/list-data" class="block px-4 py-2 rounded hover:bg-gray-700">
+            List Data
+          </a>
 
-<a href="/artikel" class="block px-4 py-2 rounded hover:bg-gray-700">
-Activity
-</a>
+          <a href="/artikel" class="block px-4 py-2 rounded hover:bg-gray-700">
+            Activity
+          </a>
 
-<a href="/contact" class="block px-4 py-2 rounded hover:bg-gray-700">
-Log Out
-</a>
+          <a href="/contact" class="block px-4 py-2 rounded hover:bg-gray-700">
+            Log Out
+          </a>
 
 
 
-</div>
+        </div>
 
-</div>
+  </div>
 
 <!-- CONTENT -->
-<div class="flex-1 bg-gray-100">
+  <div class="flex-1 bg-gray-100">
 
-<header class="bg-white shadow p-4 flex justify-between items-center">
+    <header class="bg-white shadow p-4 flex justify-between items-center">
 
-<h1 class="text-xl font-bold">Dashboard</h1>
+        <h1 class="text-xl font-bold">Dashboard</h1>
 
-<div class="relative" x-data="{ open:false }">
+          <div class="relative" x-data="{ open:false }">
 
-<button @click="open=!open">
-<img class="size-8 rounded-full"
-src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e">
-</button>
+            <button @click="open=!open">
+              <img class="size-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e">
+            </button>
 
-<div x-show="open"
-@click.outside="open=false"
-class="absolute right-0 mt-2 w-40 bg-white shadow rounded">
+                <div x-show="open"
+                @click.outside="open=false"
+                  class="absolute right-0 mt-2 w-40 bg-white shadow rounded">
 
-<a href="#" class="block px-4 py-2 hover:bg-gray-100">Profile</a>
-<a href="#" class="block px-4 py-2 hover:bg-gray-100">Settings</a>
-<a href="#" class="block px-4 py-2 hover:bg-gray-100">Sign Out</a>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-100">Profile</a>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-100">Settings</a>
+                  <a href="#" class="block px-4 py-2 hover:bg-gray-100">Sign Out</a>
 
-</div>
+                </div>
 
-</div>
+          </div>
 
-</header>
+    </header>
 
-<main class="p-6">
-<!-- isi halaman -->
-</main>
+     <main class="p-6">
+      <!-- isi halaman -->
+      </main>
 
-</div>
+  </div>
 
 </div>
