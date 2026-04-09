1731f24107e2e9c9e2cd8d1e05fbd9d23fba665c
Lyonx12 <Handose123@gmail.com>
2026-04-09 14:43:58 +0700

    chore: minor change for PR

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 
 <header class="bg-white shadow-sm">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
+        <h1 class="text-3xl font-bold text-gray-900">Dashboard Page</h1>
     </div>
 </header>
 
